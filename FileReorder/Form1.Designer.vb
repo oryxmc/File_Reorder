baa702f48ab7743de3b945f044a42dab9349217e
oryxmc <oryx_mc_wolf@yahoo.com>
2014-12-17 18:41:43 +0200

    Initial Rev

---
+++ FileReorder/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.fbBrowser = New System.Windows.Forms.FolderBrowserDialog()
+        Me.btnPickFolder = New System.Windows.Forms.Button()
+        Me.lvFiles = New System.Windows.Forms.ListView()
+        Me.btnReorder = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'fbBrowser
+        '
+        Me.fbBrowser.RootFolder = System.Environment.SpecialFolder.MyComputer
+        '
+        'btnPickFolder
+        '
+        Me.btnPickFolder.Location = New System.Drawing.Point(28, 111)
+        Me.btnPickFolder.Name = "btnPickFolder"
+        Me.btnPickFolder.Size = New System.Drawing.Size(75, 23)
+        Me.btnPickFolder.TabIndex = 0
+        Me.btnPickFolder.Text = "Folder..."
+        Me.btnPickFolder.UseVisualStyleBackColor = True
+        '
+        'lvFiles
+        '
+        Me.lvFiles.AllowDrop = True
+        Me.lvFiles.Location = New System.Drawing.Point(132, 12)
+        Me.lvFiles.Name = "lvFiles"
+        Me.lvFiles.ShowItemToolTips = True
+        Me.lvFiles.Size = New System.Drawing.Size(283, 475)
+        Me.lvFiles.TabIndex = 1
+        Me.lvFiles.UseCompatibleStateImageBehavior = False
+        Me.lvFiles.View = System.Windows.Forms.View.List
+        '
+        'btnReorder
+        '
+        Me.btnReorder.Location = New System.Drawing.Point(28, 140)
+        Me.btnReorder.Name = "btnReorder"
+        Me.btnReorder.Size = New System.Drawing.Size(75, 23)
+        Me.btnReorder.TabIndex = 2
+        Me.btnReorder.Text = "Reorder"
+        Me.btnReorder.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(457, 499)
+        Me.Controls.Add(Me.btnReorder)
+        Me.Controls.Add(Me.lvFiles)
+        Me.Controls.Add(Me.btnPickFolder)
+        Me.Name = "frmMain"
+        Me.Text = "File Reorder"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents fbBrowser As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents btnPickFolder As System.Windows.Forms.Button
+    Friend WithEvents lvFiles As System.Windows.Forms.ListView
+    Friend WithEvents btnReorder As System.Windows.Forms.Button
+
+End Class
---
+++ FileReorder/Form1.vb
@@ -0,0 +1,102 @@
+﻿Imports System.IO
+
+Public Class frmMain
+
+
+    Private Sub btnPickFolder_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPickFolder.Click
+
+        If fbBrowser.ShowDialog() = Windows.Forms.DialogResult.OK Then 'show dialog and wait for OK/Cancel
+
+            PopulateLV(fbBrowser.SelectedPath)
+        End If
+
+    End Sub
+
+    Private Sub PopulateLV(ByVal path As String)
+        Dim aszFileList() As String
+        Dim dFiles As New Dictionary(Of String, DateTime)
+
+        lvFiles.Clear()
+        aszFileList = Directory.GetFiles(path, "*.mp3")
+
+        For Each szFile As String In aszFileList
+            dFiles.Add(Strings.Right(szFile, szFile.Length - szFile.LastIndexOf("\") - 1), File.GetLastWriteTime(szFile))
+            'lvFiles.Items.Add(Strings.Right(szFile, szFile.Length - szFile.LastIndexOf("\") - 1))
+        Next
+
+        Dim sortedDict = (From entry In dFiles Order By entry.Value Descending).ToDictionary(Function(pair) pair.Key, Function(pair) pair.Value)
+
+        For Each currentKVP As KeyValuePair(Of String, DateTime) In sortedDict
+            lvFiles.Items.Add(currentKVP.Key)
+        Next
+
+    End Sub
+
+    Private Sub lvFiles_ItemDrag(ByVal sender As Object, ByVal e As System.Windows.Forms.ItemDragEventArgs) Handles lvFiles.ItemDrag
+        Dim myItem As ListViewItem
+        Dim myItems(sender.SelectedItems.Count - 1) As ListViewItem
+        Dim i As Integer = 0
+
+        ' Loop though the SelectedItems collection for the source.
+        For Each myItem In sender.SelectedItems
+            ' Add the ListViewItem to the array of ListViewItems.
+            myItems(i) = myItem
+            i = i + 1
+        Next
+        ' Create a DataObject containg the array of ListViewItems.
+        sender.DoDragDrop(New DataObject("System.Windows.Forms.ListViewItem()", myItems), DragDropEffects.Move)
+
+    End Sub
+
+    Private Sub lvFiles_DragEnter(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles lvFiles.DragEnter
+
+        If e.Data.GetDataPresent("System.Windows.Forms.ListViewItem()") Then
+            e.Effect = DragDropEffects.Move
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+
+    End Sub
+
+    Private Sub lvFiles_DragDrop(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles lvFiles.DragDrop
+
+        Try
+            Dim i As Integer
+            Dim l_Point As Point = lvFiles.PointToClient(New Point(e.X, e.Y))
+            Dim l_Item As ListViewItem = lvFiles.GetItemAt(l_Point.X, l_Point.Y)
+            Dim l_Items() As ListViewItem = e.Data.GetData("System.Windows.Forms.ListViewItem()")
+            Dim l_DropIndex As Integer = l_Item.Index
+
+            For Each l_Item In l_Items
+                lvFiles.Items.Remove(lvFiles.SelectedItems.Item(0))
+            Next
+
+            For Each l_Item In l_Items
+                lvFiles.Items.Insert(l_DropIndex + i, l_Items(i))
+                i = i + 1
+            Next
+        Catch ex As Exception
+        End Try
+
+    End Sub
+
+    Private Sub btnReorder_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnReorder.Click
+
+        Dim dtCreation = New DateTime(2012, 1, 1, 1, 1, 1)
+
+
+
+
+        For Each lviFileName As ListViewItem In lvFiles.Items
+            File.SetCreationTime(fbBrowser.SelectedPath & "\" & lviFileName.Text, dtCreation)
+            File.SetLastWriteTime(fbBrowser.SelectedPath & "\" & lviFileName.Text, dtCreation)
+            File.SetLastAccessTime(fbBrowser.SelectedPath & "\" & lviFileName.Text, dtCreation)
+            dtCreation = dtCreation.AddSeconds(-1)
+        Next
+
+
+        PopulateLV(fbBrowser.SelectedPath)
+        MsgBox("done")
+
+    End Sub
+End Class
